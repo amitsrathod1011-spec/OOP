4cb06d3964da0527d083020e420f1e1f42ff2aa8
Amit Rathod <amitsrathod1011@gmail.com>
2026-01-15 18:18:20 +0530

    Initial Java Program

---
+++ src/MainClass.java
@@ -0,0 +1,83 @@
+//laptop
+public class MainClass {
+	public static void main(String[] args) {
+		
+		laptop hp=new laptop();
+		hp.lapName();
+		hp.value();
+	}
+}
+
+
+
+
+
+
+//-------------------------------------------------
+//employee
+/*public  class MainClass{
+	public static void main(String[] args) {
+		
+		laptop emp =new laptop();
+		emp.empId();
+		emp.empSalary();
+		emp.empName();
+	}
+}
+
+
+*/
+
+
+
+
+//----------------------------------------------------------
+
+//car
+
+
+/*public class MainClass{
+	public static void main(String[] args) {
+		laptop mahindra=new laptop();
+		mahindra.model();
+		mahindra.price();
+		mahindra.color();
+	}
+}
+*/
+
+
+
+
+//------------------------------------------------------------
+
+//Book
+
+/*public class MainClass{
+	public static void main(String[] args) {
+		laptop book=new laptop();
+		book.title();
+		book.price();
+		book.author();
+	}
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/laptop.java
@@ -0,0 +1,93 @@
+//laptop
+public class laptop {
+	 String lapName;
+	 int value;
+	 String Brand;
+	 
+	 void lapName() {
+		 System.out.println("Hp");
+	 }
+	 void value() {
+		 System.out.println("55000");
+	 }
+}
+
+
+//-------------------------------------------------
+//employee
+/*public class laptop{
+	int empId;
+	String empName;
+	int empSalary;
+	
+	void empId() {
+		System.out.println("0123");
+	}
+	void empName() {
+		System.out.println("Ram");
+	}void empSalary(){
+		System.out.println("45000");
+	}
+	
+}*/
+
+//-------------------------------------------------
+
+//car
+ /*public class laptop{
+	 char model;
+	 int price;
+	 String color;
+	 
+	 void model() {
+		 System.out.println("SUV100");
+	 }
+	 void price() {
+	 System.out.println("2000000");
+ }
+	 void color() {
+		 System.out.println("black");
+	 }
+ } 
+	*/ 
+ 
+//-------------------------------------------------------------
+
+ //Book
+ /*public class laptop{
+	 String title;
+	 int price;
+	 String author;
+	 
+	 void title() {
+		 System.out.println("Java Basic");
+	 }
+	 void price() {
+	 System.out.println("2000000");
+ }
+	 void author() {
+		 System.out.println("James Gosling ");
+	 }
+ } 
+ */
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+	 
+	 
+	 
+	 
